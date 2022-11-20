6ac34f9c8a99e40e725d86c4be0294437d839b52
Max Casteel <maximilian.casteel@umconnect.umt.edu>
2022-11-20 15:08:48 -0700

    First commit as a test.

---
+++ kruskalClass.py
@@ -0,0 +1,7 @@
+#Max Casteel
+#November 18, 2022
+#Programming Assignment 1 - Kruskals Algouithnm to find the MST of a graph using an adjacency matrix.
+#
+#Advanced Algorithm
+#obj = kruskalClass()
+#Input:
