6a8f5e55922563eeb11a5599395ff1ab561e4f8c
Max Casteel <maximilian.casteel@umconnect.umt.edu>
2022-11-22 11:51:15 -0700

    Fixed array indexes. and the 5th test

--- Project_1_Advanced_Algorithms/kruskalClass.py
+++ Project_1_Advanced_Algorithms/kruskalClass.py
@@ -4,20 +4,21 @@
 
 import numpy as np
 import numpy
+import kruskalClass
  #import numpy for matrix operations
-
-
- 
 class kruskalClass:
     def __init__(self):
+        #Just a quit test to see if the object was created correctly.
         print("kruskalClass object created")    
 
     #s = find(u,v)
     #Input: 'u' is a union-find structure.  'v' is a numerical index of a graph node.
     #Output: 's' is the muerical value corresponding to the label for the set of connected nodes to which 'v' belongs.
     def find(self,u,v):
+        #if the node is the root of the tree, then return the node.
         if u[v][0] == v:
             return v
+            #if the node is not the root of the tree, then recursively call the find function on the parent node.
         else:
             return self.find(u,u[v][0])
 
@@ -26,14 +27,19 @@ class kruskalClass:
     #coresponding to the labels of two groups of graph nodes.
     #Output: 'u_out' is a union-find data structure that is the result of merging the two sets of nodes.
     def union(self,u_in,s1,s2):
+        #checks to see if the two sets are already in the same connected component.
+        if self.find(u_in,s1) == self.find(u_in,s2):
+            return u_in
+        #if the two sets are not in the same connected component, then the smaller set is added to the larger set.
         if u_in[s1][1] > u_in[s2][1]:
             u_in[s2][0] = s1
             return u_in
         elif u_in[s1][1] < u_in[s2][1]:
             u_in[s1][0] = s2
             return u_in
+            #if the two sets are the same size, then the second set is added to the first set, and the size of the first set is incremented by 1.
         else:
-            u_in[s2][0] = s1
+            u_in[s1][0] = s2
             u_in[s1][1] += 1
             return u_in
     
@@ -47,6 +53,7 @@ class kruskalClass:
     def makeUnionFind(self,N):  
         #u is a 1xN pytohn dictionary
         u = {}
+        #for loop to creat an 1xN numpy array for each node in the graph.
         for i in range(N):
             u[i] = np.array([i,1])
         return u
@@ -57,12 +64,17 @@ class kruskalClass:
     #Output: 'b' is a 1xK numpy array with elements in ascending order. (ie. b[0] <= b[1] <= ... <= b[K-1] ==> lowest value to highest value)
     def mergesort(self,a):
         if len(a) > 1:
+            #split the array into two halves
             mid = len(a)//2
             left = a[:mid]
             right = a[mid:]
+            #recursively call mergesort on the left and right halves of the array.
             self.mergesort(left)
             self.mergesort(right)
-            i = j = k = 0
+            #intialize index variables.
+            i = 0
+            j = 0
+            k = 0
             while i < len(left) and j < len(right):
                 if left[i][2] < right[j][2]:
                     a[k] = left[i]
@@ -91,8 +103,10 @@ class kruskalClass:
     #We will only consider undirected graphs, and therefore 'T' should be an upper right triangular matrix.
 
     def findMinimumSpanningTree(self,A):
+        #create a union-find data structure for the graph.
         N = A.shape[0]
         u = self.makeUnionFind(N)
+        #create a list initialized to all zeros.
         T = numpy.zeros((N,N))
         edges = []
         for i in range(N):
